11b6db1d26def17e4a9f4242d9a7e973612e1ae7
Khanh Pham <khanhpham7003@gmail.com>
2025-04-13 15:45:47 +0700

    Add testcase for BaiTap1

--- src/BaiTap1/NhanVien.java
+++ src/BaiTap1/NhanVien.java
@@ -1,4 +1,21 @@
 package BaiTap1;
 
 public class NhanVien {
+
+    String name = "Khanh";
+    int age = 29;
+    String address = "HCM";
+    String role = "QA";
+
+    public void printInfo() {
+        System.out.println("Tên: " + name);
+        System.out.println("Tuổi: " + age);
+        System.out.println("Địa chỉ: " + address);
+        System.out.println("Chức vụ: " + role);
+    }
+
+    public static void main(String[] args) {
+        NhanVien nhanVien = new NhanVien();
+        nhanVien.printInfo();
+    }
 }
--- src/BaiTap1/ThongTin.java
+++ src/BaiTap1/ThongTin.java
@@ -1,4 +1,8 @@
 package BaiTap1;
 
 public class ThongTin {
+    public static void main(String[] args) {
+        NhanVien nhanVien = new NhanVien();
+        nhanVien.printInfo();
+    }
 }
