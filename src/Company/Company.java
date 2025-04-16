b801c5c138c02325de569d229c4fe3820d113442
Khanh Pham <khanhpham7003@gmail.com>
2025-04-16 10:24:41 +0700

    BT Java OOP 1+2

---
+++ src/Company/Company.java
@@ -0,0 +1,12 @@
+package Company;
+
+import Person.Person;
+
+public class Company extends Person {
+    public static void main(String[] args) {
+        Company company = new Company();
+        company.getName();
+        company.getage();
+        company.getGender();
+    }
+}
---
+++ src/Person/Information.java
@@ -0,0 +1,9 @@
+package Person;
+
+public class Information {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.getInfo();
+
+    }
+}
